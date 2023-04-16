fa132c8dd4eeca2fd61ea6693d74af04532f4f38
ysnsales <ysnsaless@gmail.com>
2023-04-16 17:56:40 -0300

    Rota POST messages

--- src/app.js
+++ src/app.js
@@ -22,14 +22,15 @@ mongoClient
   .catch((err) => console.log(err.message));
 
 //Endpoints
+
 app.post("/participants", async (req, res) => {
     const { name } = req.body
 
-    const receitaSchema = joi.object({
+    const participantSchema = joi.object({
         name: joi.string().required()
     })
 
-    const validation = receitaSchema.validate(req.body, { abortEarly: false })
+    const validation = participantSchema.validate(req.body, { abortEarly: false })
 
     if (validation.error) {
         const errors = validation.error.details.map(detail => detail.message)
@@ -59,6 +60,36 @@ app.get("/participants", async (req, res) => {
     try {
         const participants = await db.collection("participants").find().toArray()
         res.send(participants)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/messages", async (req, res) => {
+    
+    const { to, text, type } = req.body;
+    const from = req.headers.user;
+
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid("message", "private_message").required()
+    })
+
+    const validation = messageSchema.validate(req.body, { abortEarly: false })
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        return res.status(422).send(errors)
+    }
+
+    try {
+        const participant = await db.collection("participants").findOne({name : from})
+        if (!participant) {
+            return res.status(422)
+        }
+        const message = {from, to, text, type, time: dayjs().format("HH:mm:ss")}
+        await db.collection("messages").insertOne(message)
+        res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
     }
