84ff58ee8f879c61d66486c0a8776040e6bb02f1
ysnsales <ysnsaless@gmail.com>
2023-04-16 20:34:08 -0300

    Ajustes: Rota POST messages

--- src/app.js
+++ src/app.js
@@ -70,6 +70,8 @@ app.post("/messages", async (req, res) => {
     const { to, text, type } = req.body;
     const from = req.headers.user;
 
+    if (!from || from === null) return res.sendStatus(422)
+
     const messageSchema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
