13f534fc083fef208b388473f12cd0f0e01eff59
ysnsales <ysnsaless@gmail.com>
2023-04-16 14:28:21 -0300

    Rota POST parcipants

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient, ObjectId } from "mongodb"
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv"
-import joi from "joi"
+import joi from "joi";
+import dayjs from "dayjs";
 
 //Criação do APP Servidor
 const app = express();
@@ -11,3 +12,49 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
+
+//Conexão com o banco de dados
+let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+mongoClient
+  .connect()
+  .then(() => (db = mongoClient.db()))
+  .catch((err) => console.log(err.message));
+
+//Endpoints
+app.post("/participants", async (req, res) => {
+    const { name } = req.body
+
+    const receitaSchema = joi.object({
+        name: joi.string().required()
+    })
+
+    const validation = receitaSchema.validate(req.body, { abortEarly: false })
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        return res.status(422).send(errors)
+    }
+
+    try {
+        const participant = await db.collection("participants").findOne({ name: name })
+        if (participant) return res.status(409).send("Esse nome já está em uso.")
+
+        await db.collection("participants").insertOne({name, lastStatus:Date.now()})
+
+        const message = {from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().hour() 
+        }
+        await db.collection("messages").insertOne(message)
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+// Deixa o app escutando, à espera de requisições
+const PORT = 5000
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`)) 
