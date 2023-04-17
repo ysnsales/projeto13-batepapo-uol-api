057b516429fe2ad59fd4cf8d72388cca5f94781a
ysnsales <ysnsaless@gmail.com>
2023-04-16 21:11:27 -0300

    Bônus: Rota PUT messages

--- src/app.js
+++ src/app.js
@@ -120,7 +120,8 @@ app.get("/messages", async (req, res) => {
 })
 
 app.delete("/messages/:id", async (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
+    const { to, text, type } = req.body;
     const user = req.headers.user;
 
     try {
@@ -131,6 +132,38 @@ app.delete("/messages/:id", async (req, res) => {
         await db.collection("messages").deleteOne({ _id: new ObjectId(id)  })
         res.send("Item deletado com sucesso!")
 
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.put("/messages/:id", async (req, res) => {
+    const { id } = req.params;
+    const from = req.headers.user;
+
+    if (!from || from === null) return res.sendStatus(422)
+
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid("message", "private_message").required()
+    })
+
+    const validation = messageSchema.validate(req.body, { abortEarly: false })
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        return res.status(422).send(errors)
+    }
+
+    try {
+        const participant = await db.collection("participants").findOne({name : from})
+        if (!participant) return res.sendStatus(422)
+        
+        const result = await db.collection("messages").updateOne(
+            { _id: new ObjectId(id) },
+            { $set: req.body }
+        )
+        if (result.matchedCount === 0) return res.sendStatus(404)
     } catch (err) {
         res.status(500).send(err.message)
     }
